460b7a464425dbc2975d53945d321fb6760169d3
SHUBHAM PATIL <SHUBHAM PATIL@DESKTOP-JV917LS>
2024-10-11 11:12:51 +0530

    Add project files.

---
+++ StudentPortalWeb/Models/Page.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentPortalWeb.Models
+{
+    public class Page
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+    }
+}
---
+++ StudentPortalWeb/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentPortalWeb.Models
+{
+    public class Post
+    {
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        public string? ShortDescription { get; set; }
+
+        public string? ApplicationUserId { get; set; }
+        //relation
+        public ApplicationUser? ApplicationUser { get; set; }
+
+        public DateTime? Created { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public string? Description { get; set; }
+
+        public string? slug { get; set; }
+
+
+    }
+}
